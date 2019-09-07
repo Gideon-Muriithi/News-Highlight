9ab8064bb1e8bda28486201f4a71fb30354d30ad
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-07 09:17:46 +0300

    This commit adds a function that proccesses api request and returns highlight objects from elements neeeded

--- app/requests.py
+++ app/requests.py
@@ -14,7 +14,6 @@ base_url = app.config["ARTICLES_API_BASE_URL"]
 def get_highlights(category):
     '''We then use with as our context manager to send a request using theurllib.request.urlopen() function that takes in the get_movies_url as an argument and sends a request as url
 
-
     Function that gets the json response to our url request
     '''
     get_highlights_url = base_url.format(category, api_key)
@@ -31,3 +30,29 @@ def get_highlights(category):
 
 
     return highlight_results
+
+    def process_results(highlight_list):
+        '''
+        Function  that processes the news highlights result and transform them to a list of Objects
+
+        Args:
+            highlight_list: A list of dictionaries that contain highlight details
+
+        Returns :
+            highlight_results: A list of highlight objects
+        '''
+        highlight_results = []
+
+        for highlight_item in highlight_list:
+            source = source_item.get('source.name')
+            title = highlight_item.get('title')
+            description = highlight_item.get('description')
+            url = highlight_item.get('url')
+            urlToImage = highlight_item.get('urlToImage')
+            publishedAt = highlight_item.get('publishedAt')
+
+            if urlToImage:
+                highlight_object = NewsHighlight(source, title, description, url, urlToImage, publishedAt)
+                highlight_results.append(highlight_object)
+
+        return highlight_results
